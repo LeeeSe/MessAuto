ca4909db24830e0084abd3098d1fb202b945dcb2
LeeeSe <724818342@qq.com>
2025-08-19 13:18:26 +0800

    change log to i18n

--- src/updater.rs
+++ src/updater.rs
@@ -1,6 +1,7 @@
 use crate::notification;
 use cargo_packager_updater::{Config, Update, check_update, semver::Version};
 use log::{error, info};
+use rust_i18n::t;
 use std::env;
 use std::thread;
 
@@ -20,7 +21,7 @@ fn get_endpoint() -> String {
         "https://github.com/LeeeSe/MessAuto/releases/latest/download/update-{}.json",
         arch
     );
-    info!("检查更新的 endpoint: {}", endpoint);
+    info!("{}", t!("updater.endpoint_for_update", endpoint));
     endpoint
 }
 
@@ -30,13 +31,13 @@ const PUB_KEY: &str = "dW50cnVzdGVkIGNvbW1lbnQ6IG1pbmlzaWduIHB1YmxpYyBrZXk6IDY2N
 
 pub fn check_for_updates() {
     thread::spawn(move || {
-        info!("开始检查更新...");
-        info!("当前架构: {}", get_current_arch());
+        info!("{}", t!("updater.checking_for_updates"));
+        info!("{}", t!("updater.current_architecture", get_current_arch()));
 
         let current_version = match Version::parse(CURRENT_VERSION) {
             Ok(version) => version,
             Err(e) => {
-                error!("解析当前版本失败: {}", e);
+                error!("{}", t!("updater.failed_to_parse_current_version", e));
                 return;
             }
         };
@@ -51,68 +52,65 @@ pub fn check_for_updates() {
 
         match check_update(current_version, config) {
             Ok(Some(update)) => {
-                info!("发现新版本: {}", update.version);
+                info!("{}", t!("updater.new_version_found", update.version));
                 match download_update(update) {
                     Ok((update_obj, update_bytes)) => {
-                        info!("更新下载完成，准备安装");
+                        info!("{}", t!("updater.update_download_complete_ready_to_install"));
                         if show_install_notification(&update_obj.version) {
                             if let Err(e) = install_update(update_obj, update_bytes) {
-                                error!("安装更新失败: {}", e);
+                                error!("{}", t!("updater.failed_to_download_update", e));
                             }
                         } else {
-                            info!("用户取消安装更新");
+                            info!("{}", t!("updater.user_cancelled_update_installation"));
                         }
                     }
                     Err(e) => {
-                        error!("下载更新失败: {}", e);
+                        error!("{}", t!("updater.failed_to_download_update", e));
                     }
                 }
             }
             Ok(None) => {
-                info!("应用已是最新版本。");
+                info!("{}", t!("updater.already_up_to_date"));
             }
             Err(e) => {
-                error!("检查更新失败: {}", e);
+                error!("{}", t!("updater.failed_to_check_for_updates", e));
             }
         }
     });
 }
 
 fn download_update(update: Update) -> Result<(Update, Vec<u8>), Box<dyn std::error::Error>> {
-    info!("正在后台下载更新...");
+    info!("{}", t!("updater.downloading_update_in_background"));
 
     let update_bytes = update.download()?;
-    info!("更新下载完成");
+    info!("{}", t!("updater.update_download_complete"));
 
     Ok((update, update_bytes))
 }
 
 fn install_update(update: Update, update_bytes: Vec<u8>) -> Result<(), Box<dyn std::error::Error>> {
-    info!("正在安装更新...");
+    info!("{}", t!("updater.installing_update"));
 
     // 安装更新
     update.install(update_bytes)?;
-    info!("更新安装成功，应用将重启");
+    info!("{}", t!("updater.update_installed_successfully_app_will_restart"));
 
     Ok(())
 }
 
 fn show_install_notification(version: &str) -> bool {
-    info!("新版本 {} 已下载完成，询问用户是否安装", version);
+    info!("{}", t!("updater.new_version_downloaded_ask_user_to_install", version));
 
     // 使用系统通知询问用户是否要安装更新
-    let title = "MessAuto 更新可用";
-    let content = format!(
-        "新版本 {} 已下载完成。是否现在安装更新？\n\n安装后应用将自动重启。",
-        version
-    );
+    let title = t!("updater.update_available_dialog_title");
+    let content = t!("updater.update_available_dialog_content", version);
 
-    let user_choice = notification::dialog(title, &content, "安装", "稍后");
+    let user_choice = notification::dialog(&title, &content, "安装", "稍后");
 
     if user_choice {
-        info!("用户选择安装更新");
+        info!("{}", t!("updater.user_chosen_to_install_update"));
     } else {
-        info!("用户选择稍后安装更新");
+        info!("{}", t!("updater.user_chosen_to_install_update_later"));
     }
 
     user_choice
