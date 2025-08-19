c385000c91a8a641a7d4a6ba7f38b13de60a5891
LeeeSe <724818342@qq.com>
2025-08-19 22:17:39 +0800

    Bump version to 1.1.2 and remove test_locale.rs

--- test_locale.rs
+++
@@ -1,8 +0,0 @@
-use sys_locale::get_locale;
-
-fn main() {
-    match get_locale() {
-        Some(locale) => println!("System locale: {}", locale),
-        None => println!("Could not determine system locale"),
-    }
-}
